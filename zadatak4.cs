6214c4ca152dc9f9ddbbbdc4d685f046b4d558b2
Kristian75 <152974653+Kristian75@users.noreply.github.com>
2024-01-30 13:01:45 +0100

    Add files via upload

---
+++ zadatak4.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak4
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            int a, b, c, d;
+
+            Console.WriteLine("Upisi broj a:");
+
+            Console.WriteLine("Upisi broj b:");
+
+            Console.WriteLine("Upisi broj c:");
+
+            d = (a + b + c);
+
+            if{ 
+                d % 2;
+
+                Console.WriteLine("DA");
+             }
+            else
+            {
+                Console.WriteLine("NE");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
